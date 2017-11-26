ee60854094b241cff6656a9798eedd5cc00ce2f4
Marto <martinkaldramov1@gmail.com>
2017-11-26 21:14:17 +0200

    Finished the create_company_acc function

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,9 +1,21 @@
-const Job = require('../models/job'),
-      Company = require('../models/companyAcc.js');
+// const Job = require('../models/job');
+const Company = require('../models/companyAcc.js');
 
-/*eslint-disable */
-module.exports.create_company_acc = (req, res, next) => { /*eslint-enable */
-  var company = new Company(() => {
-    
+
+module.exports.create_company_acc = (req, res, next) => { // eslint-disable-line
+  var company = new Company({
+    username: req.body.username,
+    password: req.body.password,
+    accType: req.body.accType,
+    name: req.body.name,
+    address: req.body.address,
+    phone: req.body.phone,
+    webUrl: req.body.webUrl,
   });
+
+  company.save().then((res) => {
+    res.send(res);
+  }).catch((e) => {
+    console.log(e); // eslint-disable-line
+  })
 }
