ce97036a4c305fac5bd0c3bf82bf2614c70bd875
Marto <martinkaldramov1@gmail.com>
2017-12-16 19:34:51 +0200

    Included the routes in app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const path = require('path'); // eslint-disable-line
 const {ObjectID} = require('mongodb'); // eslint-disable-line
 
+const accounts = require('./routes/accountsRouter');
+const jobs = require('./routes/jobsRouter');
+
 // const {mongoose} = require('./db/mongoose');
 
 const app = express();
@@ -13,5 +16,8 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('Working');
 });
+
+app.use('/jobs', jobs);
+app.use('/accounts', accounts);
 
 module.exports = app;
