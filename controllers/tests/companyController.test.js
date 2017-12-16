4bd439399124baf8068fe398cfab13eb144fa183
Marto <martinkaldramov1@gmail.com>
2017-12-16 17:41:54 +0200

    Setting up the controller tests with Tape

---
+++ controllers/tests/companyController.test.js
@@ -0,0 +1,2 @@
+const test = require('tape'); // eslint-disable-line
+const jobCtrl = require('./../jobController.js');
---
+++ controllers/tests/jobController.test.js
@@ -0,0 +1,2 @@
+const test = require('tape'); // eslint-disable-line
+const companyCtrl = require('./../companyController.js');
